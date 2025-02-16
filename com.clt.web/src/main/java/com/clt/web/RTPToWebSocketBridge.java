4cdc3e6eabbaf66dc91f1bc6cca1ab470dfa56c3
AbdulaAlalian <abdula.alalian@web.de>
2025-02-16 11:26:44 +0100

    Disabled timeout, changed index.html title

--- com.clt.web/src/main/java/com/clt/web/RTPToWebSocketBridge.java
+++ com.clt.web/src/main/java/com/clt/web/RTPToWebSocketBridge.java
@@ -117,8 +117,8 @@ public class RTPToWebSocketBridge {
     @OnWebSocketConnect
     public void onConnect(Session session) {
         System.out.println("WebSocket Connected: " + session.getRemoteAddress());
-        // timeout after not receiving data for 10 minutes (should be enough)
-        session.setIdleTimeout(600000);
+        // disable timeout
+        session.setIdleTimeout(0);
         sessions.add(session);
     }
 
